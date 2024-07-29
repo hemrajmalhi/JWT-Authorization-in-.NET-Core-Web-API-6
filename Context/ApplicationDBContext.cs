3c2321c3411ddde92b91582562c2ed0b4c491f51
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2024-07-29 17:48:19 +0500

    Add project files.

---
+++ Context/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using JwoToken.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwoToken.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<User> User { get; set; }
+    }
+}
